5a30aeed4a7bf9a47e7b67d438acd9b6fe751d9f
HeechanYang <yhc944@gmail.com>
2019-07-24 21:19:06 +0900

    [ADD] ArrayList

--- Algorithm/src/datastructures/lists/ArrayList.java
+++ Algorithm/src/datastructures/lists/ArrayList.java
@@ -1,13 +1,82 @@
 package datastructures.lists;
 
-public class ArrayList {
-	
-	
+import java.util.Arrays;
+
+public class ArrayList<T> {
+	private static final int DEFAULT_CAPACITY = 100000;
+	private static final float LOAD_FACTOR = 0.75f;
+
+	private T[] arr;
+	private int capacity;
+	private int size;
+
+	public ArrayList() {
+		this(DEFAULT_CAPACITY);
+	}
+
+	@SuppressWarnings("unchecked")
+	public ArrayList(int capacity) {
+		this.arr = (T[]) new Object[capacity];
+		this.capacity = DEFAULT_CAPACITY;
+		this.size = 0;
+	}
+
+	public boolean isEmpty() {
+		return this.size == 0;
+	}
+
+	public boolean needExtend() {
+		return this.size > this.capacity * LOAD_FACTOR;
+	}
+
+	public void add(T t) {
+		this.arr[++this.size - 1] = t;
+	}
+
+	public void add(int idx, T t) {
+		for (int i = this.size++; i > idx; i--) {
+			this.arr[i] = this.arr[i - 1];
+		}
+		this.arr[idx] = t;
+	}
+
+	public int size() {
+		return this.size;
+	}
+
+	public T get(int idx) {
+		return arr[idx];
+	}
+
+	public T remove(int idx) {
+		T temp = arr[idx];
+
+		for (int i = idx; i < size; i++) {
+			this.arr[i] = this.arr[i + 1];
+		}
+		size--;
+		return temp;
+	}
+
+	// just for test
+	public void print() {
+		System.out.println(Arrays.toString(this.arr));
+	}
+
 	public static void main(String[] args) {
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
+		ArrayList<Integer> list = new ArrayList();
+
+		list.add(1);
+		list.add(2);
+		list.add(1,3);
+		list.add(4);
+		
+		list.print();
+		
+		list.remove(2);
+		
+		list.print();
+		
+		System.out.println(list.size());
 	}
 }
--- Algorithm/src/swexpertacademy/P7854.java
+++ Algorithm/src/swexpertacademy/P7854.java
@@ -1,37 +1,46 @@
 package swexpertacademy;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class P7854 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
 
-		int T = Integer.parseInt(sc.nextLine());
+			int T = Integer.parseInt(br.readLine());
+			StringBuilder sb = new StringBuilder();
 
-		int[] front = new int[] { 1, 2, 5, 10, 20, 25, 50 };
-		int[] temp = new int[] { 100, 125, 200, 250, 500 };
-		int[] fin = new int[43];
-		for (int i = 0; i < 43; i++) {
-			if (i < front.length) {
-				fin[i] = front[i];
-			} else {
-				int i7 = i - 7;
-				fin[i] = temp[i7 % 5];
-				for (int j = 0; j < i7 / 5; j++) {
-					fin[i] *= 10;
+			int[] front = new int[] { 1, 2, 5, 10, 20, 25, 50 };
+			int[] temp = new int[] { 100, 125, 200, 250, 500 };
+			int[] fin = new int[43];
+			for (int i = 0; i < 43; i++) {
+				if (i < front.length) {
+					fin[i] = front[i];
+				} else {
+					int i7 = i - 7;
+					fin[i] = temp[i7 % 5];
+					for (int j = 0; j < i7 / 5; j++) {
+						fin[i] *= 10;
+					}
 				}
 			}
+
+			for (int t = 1; t <= T; t++) {
+				int num = Integer.parseInt(br.readLine());
+
+				int i = 0;
+				for (i = 0; i < fin.length && num >= fin[i]; i++)
+					;
+
+				sb.append(String.format("#%d %d\n", t, i));
+			}
+			
+			bw.write(sb.toString());
+			bw.flush();
 		}
-
-		for (int t = 1; t <= T; t++) {
-			int num = Integer.parseInt(sc.nextLine());
-
-			int i = 0;
-			for (i = 0; i < fin.length && num >= fin[i]; i++);
-
-			System.out.printf("#%d %d\n", t, i);
-		}
-
-		sc.close();
 	}
 }
